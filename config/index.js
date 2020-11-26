8fc1d5b595d4d4546d23c12c678350bedee74493
Dark <slightlove.u@gmail.com>
2020-11-26 20:10:46 +0800

    修改衝突

--- config/index.js
+++ config/index.js
@@ -8,10 +8,7 @@ module.exports = {
   dev: {
 
     // Paths
-<<<<<<< HEAD
     assetsRoot: path.resolve(__dirname, './app/'),
-=======
->>>>>>> 8c8ac4eb34182153ae500e78e204442ef4759574
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
